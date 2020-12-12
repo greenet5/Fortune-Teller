43b993bac9c655f0545cd5258b5597e540a559fe
Greenet5 <tyleragreene5@gmail.com>
2020-12-12 12:26:36 -0600

    Added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+/*
+Plan:
+This project will be built in JavaScript. 
+The end goal is to build a fortune teller. 
+When the user runs the program, it will output an initial message, something like "hmmmm, you're difficult to read". 
+The next message to output will state the the outlook for the user, something like "things don't look good for you". 
+The following message will state a short term fate for the user, something like "The next few days will be dark". 
+The following message will output a long term fate for the user, something like "but you will live a very long and happy life".
+The final message will be something like "Now shoo. Run along". 
+*/
+
